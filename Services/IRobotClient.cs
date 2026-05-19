999f9a4ef1279d544004de7475602c4a3ca46d90
25815784 <25815784@students.lincoln.ac.uk>
2026-05-19 15:13:47 +0100

    Change GetStatusAsync return type to RobotStatusDto

--- Services/IRobotClient.cs
+++ Services/IRobotClient.cs
@@ -1,6 +1,6 @@
 public interface IRobotClient
 {
-    Task<string> GetStatusAsync();
+    Task<RobotStatusDto> GetStatusAsync();
     Task<bool> MoveAsync(int x, int y);
     Task<bool> ResetAsync();
 }
